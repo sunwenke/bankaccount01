139b07302ed5b41408b1e17f71ccbcaf7c2c4424
孙文轲 <sunwenke2009@126.com>
2018-03-22 14:47:07 +0800

    devise

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,4 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user!
   def index
     @groups = Group.all
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321235302) do
+ActiveRecord::Schema.define(version: 20180322063301) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string "title"
+    t.string "account"
+    t.string "location"
+    t.string "bank"
+    t.string "money"
+    t.string "yougtu"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,6 +37,7 @@ ActiveRecord::Schema.define(version: 20180321235302) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
